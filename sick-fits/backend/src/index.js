cccba8b444e1742756c4e0e8c4a4504b342dc88c
Sarah Jeffery <therealsarahjeffery@gmail.com>
2020-04-21 22:09:17 -0700

    working up to video 40

--- sick-fits/backend/src/index.js
+++ sick-fits/backend/src/index.js
@@ -1,21 +1,50 @@
+const cookieParser = require('cookie-parser');
+const jwt = require('jsonwebtoken');
+
 require('dotenv').config({ path: 'variables.env' });
 const createServer = require('./createServer');
 const db = require('./db');
 
 const server = createServer();
 
-//TODO use express middleware to handle cookies(JWT)
-//TODO use express middleware to populate current user
+
+server.express.use(cookieParser());
+
+
+// decode the jwt so we can get the iuser id on each request
+server.express.use((req, res, next) => {
+  console.log(" hey im a midleware");
+  const { token } = req.cookies;
+  if (token) {
+    const { userId } = jwt.verify(token, process.env.APP_SECRET);
+    // put the userID onto the req for future request to access
+    req.userId = userId;
+  }
+  next();
+});
+
+// 2. create a middleware that populates the user on each request
+
+server.express.use(async (req, res, next) => {
+  // if they arnt logged in , skip this
+  if (!req.userId) return next();
+  const user = await db.query.user(
+    { where: { id: req.userId } },
+    '{id, permissions, email, name}'
+  );
+  req.user = user;
+  next();
+});
 
 server.start(
   {
     cors: {
-      credentails: true,
+      credentials: true,
       origin: process.env.FRONTEND_URL,
     },
   },
   deets => {
-    console.log(`Server is now running on port http://localhost${deets.port}`);
+    console.log(`Server is now running on port http://localhost:${deets.port}`);
   }
 );
 
--- sick-fits/frontend/components/Items.js
+++ sick-fits/frontend/components/Items.js
@@ -2,16 +2,21 @@ import React, { Component } from "react";
 import { Query } from "react-apollo";
 import gql from "graphql-tag";
 import styled from "styled-components";
+import Item from './Item';
+import Title from "./styles/Title";
+import Pagination from './Pagination';
+import { perPage } from '../config';
+
 
 const ALL_ITEMS_QUERY = gql`
-  query ALL_ITEMS_QUERY {
-    items {
+  query ALL_ITEMS_QUERY($skip: Int = 0, $first: Int = ${perPage}) {
+    items(first: $first, skip: $skip, orderBy: price_DESC) {
       id
       title
+      price
       description
       image
       largeImage
-      price
     }
   }
 `;
@@ -28,20 +33,35 @@ const ItemsList = styled.div`
   margin: 0 auto;
 `;
 
-export default class Items extends Component {
-  render() {
+class Items extends Component {
+  render () {
     return (
-      <div>
-        <p>Items!</p>
-        <Query query={ALL_ITEMS_QUERY}>
-          {({ data }, error, loading) => {
-            if (loading) return <p>loading...</p>;
+      <Center>
+        <Pagination page={this.props.page} />
+        <Query
+          query={ALL_ITEMS_QUERY}
+
+          variables={{
+            skip: this.props.page * perPage - perPage,
+          }}
+        >
+          {({ data, error, loading }) => {
+            if (loading) return <p>Loading...</p>;
             if (error) return <p>Error: {error.message}</p>;
             console.log(data.items, data);
-            return <ItemsList>Item</ItemsList>;
+            return (
+              <ItemsList >
+                {data.items.map(item => <Item
+                  item={item} key={item.id} />)}
+              </ItemsList>
+            );
           }}
         </Query>
-      </div>
+        <Pagination page={this.props.page} />
+      </Center>
     );
   }
 }
+
+export default Items;
+export { ALL_ITEMS_QUERY };
